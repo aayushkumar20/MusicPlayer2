e2188188136ce43d7cf67dfe25b89d0c9b6eaf65
aayushkumar20 <aayushkumarjsr16@protonmail.com>
2023-06-14 01:19:01 +0530

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Aayush kumar on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
